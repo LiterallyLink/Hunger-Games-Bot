270cd7da8eb54be94408745350714c0c1d6eb976
LinkWasTaken <zechariahpeoples@gmail.com>
2022-03-12 22:35:58 -0800

    Create HungerGamesClient.js
    Standard Client File

---
+++ src/Structures/HungerGamesClient.js
@@ -0,0 +1,38 @@
+const { Client, Collection, Intents } = require('discord.js');
+const Canvas = require('./Utilities/Canvas.js');
+const Util = require('./Utilities/Util.js');
+
+module.exports = class HungerGamesClient extends Client {
+
+	constructor(options = {}) {
+		super({
+			intents: new Intents(),
+			allowedMentions: {
+				parse: ['users']
+			}
+		});
+
+		this.validate(options);
+
+		this.slashCommands = new Collection();
+		this.events = new Collection();
+
+		this.utils = new Util(this);
+		this.cavnas = new Canvas(this);
+	}
+
+	validate(options) {
+		if (typeof options !== 'object') throw new TypeError('Options should be a type of Object.');
+
+		if (!options.token) throw new Error('You must pass the token for the client.');
+		this.token = options.token;
+	}
+
+	async start(token = this.token) {
+		await this.utils.loadSlashCommands();
+		this.utils.loadEvents();
+
+		await super.login(token);
+	}
+
+};
