f3446f65c496c67d80767db01d93935ee61321be
LinkWasTaken <zechariahpeoples@gmail.com>
2022-03-12 22:36:15 -0800

    Create ready.js

---
+++ src/Events/ready.js
@@ -0,0 +1,20 @@
+const Event = require('../Structures/Event.js');
+
+module.exports = class extends Event {
+
+	constructor(...args) {
+		super(...args, {
+			once: true
+		});
+	}
+
+	async run() {
+		console.log([
+			`Logged in as ${this.client.user.tag}`
+		].join('\n'));
+
+
+		await this.client.user.setActivity(`The Hunger Games`, { type: 'PLAYING' });
+	}
+
+};
