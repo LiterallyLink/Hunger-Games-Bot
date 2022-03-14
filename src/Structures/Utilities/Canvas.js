628a657ec6c238a7eec1e57b685758532a6a485f
LinkWasTaken <zechariahpeoples@gmail.com>
2022-03-14 14:13:40 -0700

    Update Canvas.js

--- src/Structures/Utilities/Canvas.js
+++ src/Structures/Utilities/Canvas.js
@@ -1,7 +1,25 @@
+const { loadImage } = require('canvas');
+
 module.exports = class Canvas {
 
 	constructor(client) {
 		this.client = client;
+	}
+
+	drawBackground(ctx, bgColor) {
+		ctx.fillStyle = bgColor;
+		ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+	}
+
+	async massLoadImages(tributeData) {
+		const avatarPromises = [];
+
+		for (let i = 0; i < tributeData.length; i++) {
+			const avatar = loadImage(tributeData[i].avatar);
+			avatarPromises.push(avatar);
+		}
+
+		return await Promise.all(avatarPromises);
 	}
 
 };
