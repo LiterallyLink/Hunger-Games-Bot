1280e5ae3517cc2496cbfb1ee69d78c4267dda3b
LinkWasTaken <zechariahpeoples@gmail.com>
2022-03-15 13:42:44 -0700

    Update hungergames.js

--- src/SlashCommands/hungergames.js
+++ src/SlashCommands/hungergames.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
-const { createCanvas } = require('canvas');
+const hungerGamesEvents = require('../../hungerGamesEvents.json');
+const { createCanvas, loadImage } = require('canvas');
 
 const avatarSize = 150;
 const halfAvatar = 75;
@@ -61,11 +62,74 @@ module.exports = {
 		const theReapingEmbed = new MessageEmbed()
 			.setImage('attachment://tributesPage.png')
 			.setColor('#5d5050');
-		application.followUp({
+		await application.followUp({
 			embeds: [theReapingEmbed],
 			files: [{ attachment: canvas.toBuffer(), name: 'tributesPage.png' }],
 			components: [buttons]
 		});
+
+		let bloodBath = true;
+		let sun = true;
+		let turn = 0;
+		let totalFunerals = 0;
+
+		while (!client.game.gameOver(tributeData)) {
+			if (!bloodBath && sun) turn++;
+
+			const remainingTributes = tributeData.filter(tribute => tribute.alive);
+			const currentEvent = bloodBath ? hungerGamesEvents.bloodbath : sun ? hungerGamesEvents.day : hungerGamesEvents.night;
+
+			const deaths = [];
+			const results = [];
+			const mentionResults = [];
+			const avatarsPerEvent = [];
+
+			this.eventTrigger(remainingTributes, currentEvent, deaths, results, mentionResults, avatarsPerEvent);
+
+			const eventText = `${bloodBath ? 'Bloodbath' : sun ? `Day ${turn}` : `Night ${turn}`}`;
+
+			const hungerGamesEmbed = new MessageEmbed()
+				.setTitle(`The Hunger Games - ${eventText}`)
+				.setColor('#5d5050');
+
+			for (let i = 0; i < results.length; i++) {
+				const eventImage = await this.generateEventImage(client, eventText, results[i], avatarsPerEvent[i]);
+
+				hungerGamesEmbed.setImage('attachment://currentEvent.png');
+				hungerGamesEmbed.setDescription(`${mentionResults[i]}`);
+				hungerGamesEmbed.setFooter({ text: `${remainingTributes.length} Tributes Remaining...` });
+
+				await application.followUp({ embeds: [hungerGamesEmbed], files: [{ attachment: eventImage.toBuffer(), name: 'currentEvent.png' }] });
+				await client.utils.sleep(5000);
+			}
+
+			if (deaths.length) {
+				totalFunerals++;
+				const deathMessage = `${deaths.length} cannon shot${deaths.length === 1 ? '' : 's'} can be heard in the distance.`;
+				const deadTributesImage = await this.generateFallenTributes(client, deaths, totalFunerals, deathMessage);
+
+				const deadTributesEmbed = new MessageEmbed()
+					.setTitle(`The Hunger Games - Fallen Tributes`)
+					.setImage('attachment://deadTributes.png')
+					.setDescription(`\n${deathMessage}\n\n${deaths.map(trib => `<@${trib.id}>`).join('\n')}`)
+					.setColor('#5d5050');
+				await application.followUp({ embeds: [deadTributesEmbed], files: [{ attachment: deadTributesImage.toBuffer(), name: 'deadTributes.png' }] });
+				await client.utils.sleep(5000);
+			}
+
+			if (!bloodBath) sun = !sun;
+
+			if (bloodBath) bloodBath = false;
+		}
+
+		const tributesLeft = tributeData.filter(tribute => tribute.alive);
+		const winnerName = tributesLeft.length > 1 ? `${tributesLeft.map(tribute => tribute.name).join(' and ')}` : `${tributesLeft[0].name}`;
+		const winnerSuffix = tributesLeft.length > 1 ? 'are the winners' : 'is the winner';
+
+		const winnerEmbed = new MessageEmbed()
+			.setTitle(`${winnerName} from District ${tributesLeft[0].district} ${winnerSuffix} of the Hunger Games!`)
+			.setColor('#5d5050');
+		application.followUp({ embeds: [winnerEmbed] });
 	},
 
 	generateTributeData(tributes) {
@@ -81,7 +145,7 @@ module.exports = {
 				alive: true,
 				kills: [],
 				killedBy: '',
-				district: Math.ceil((i + 1) / 2)
+				district: tributes.length === 2 ? i + 1 : Math.ceil((i + 1) / 2)
 			};
 
 			tributeDataArray.push(tributeObj);
@@ -106,6 +170,115 @@ module.exports = {
 		await this.generateStatusImage(ctx, tributeData, client);
 
 		return canvas;
+	},
+
+	async generateFallenTributes(client, deaths, totalFunerals, deathMessage) {
+		const canvasHeight = 450;
+		const canvas = createCanvas(1, canvasHeight);
+		const ctx = canvas.getContext('2d');
+		ctx.font = 'bold 28px arial';
+
+		const deathMessageLength = ctx.measureText(deathMessage).width + 200;
+		const avatarXLength = (avatarSize * deaths.length) + (avatarSpacingX * (deaths.length - 1)) + 100;
+		const canvasWidth = Math.max(deathMessageLength, avatarXLength);
+
+		ctx.canvas.width = canvasWidth;
+		client.canvas.drawBackground(ctx, '#5d5050');
+		this.drawHeaderText(ctx, deathMessage, `Fallen Tributes ${totalFunerals}`);
+
+		const avatarYPosition = (canvasHeight / 2) + 10;
+		let avatarXPosition = (canvasWidth / 2) - halfAvatar;
+		avatarXPosition -= ((avatarSpacingX / 2) + halfAvatar) * (deaths.length - 1);
+
+		for (let i = 0; i < deaths.length; i++) {
+			const tributeImage = await loadImage(deaths[i].avatar);
+			ctx.drawImage(tributeImage, avatarXPosition, avatarYPosition, avatarSize, avatarSize);
+
+			const imageData = ctx.getImageData(avatarXPosition, avatarYPosition, avatarSize, avatarSize);
+			const pixels = imageData.data;
+
+			for (let j = 0; j < pixels.length; j += 4) {
+				const lightness = parseInt((pixels[j] + pixels[j + 1] + pixels[j + 2]) / 3);
+
+				pixels[j] = lightness;
+				pixels[j + 1] = lightness;
+				pixels[j + 2] = lightness;
+			}
+
+			ctx.putImageData(imageData, avatarXPosition, avatarYPosition);
+
+			ctx.strokeRect(avatarXPosition, avatarYPosition, avatarSize, avatarSize);
+
+			avatarXPosition += avatarSpacingX + avatarSize;
+		}
+
+		return canvas;
+	},
+
+	parseEvent(text, tributes, ID) {
+		for (let i = 0; i < tributes.length; i++) {
+			const idOrName = ID ? `<@${tributes[i].id}>` : tributes[i].name;
+			text = text.replaceAll(`(Player${i + 1})`, `${idOrName}`);
+		}
+
+		return text;
+	},
+
+	// eslint-disable-next-line consistent-return
+	eventTrigger(tributeData, events, deaths, results, mentionResults, avatarsPerEvent) {
+		const tributes = new Set(tributeData);
+
+		for (const tribute of tributes) {
+			if (!tributes.has(tribute)) continue;
+
+			const filteredEvents = events.filter(event => event.tributes <= tributes.size && event.deaths < tributes.size);
+			const event = filteredEvents[Math.floor(Math.random() * filteredEvents.length)];
+
+			tributes.delete(tribute);
+
+			if (event.tributes === 1) {
+				if (event.deaths.length === 1) {
+					deaths.push(tribute);
+					tribute.alive = false;
+					tributes.delete(tribute);
+				}
+
+				results.push(this.parseEvent(event.text, [tribute], false));
+				mentionResults.push(this.parseEvent(event.text, [tribute], true));
+				avatarsPerEvent.push([tribute.avatar]);
+			} else {
+				const currTribute = [tribute];
+
+				if (event.killers.includes(1)) tribute.kills.push(tribute);
+
+				if (event.deaths.includes(1)) {
+					deaths.push(tribute);
+					tribute.alive = false;
+					tributes.delete(tribute);
+				}
+
+				for (let i = 2; i <= event.tributes; i++) {
+					const tributesArray = Array.from(tributes);
+					const randomTribute = tributesArray[Math.floor(Math.random() * tributesArray.length)];
+
+					if (event.killers.includes(i)) randomTribute.kills.push(randomTribute);
+
+					if (event.deaths.includes(i)) {
+						tribute.kills.push(randomTribute);
+						randomTribute.alive = false;
+						deaths.push(randomTribute);
+						tributes.delete(randomTribute);
+					}
+
+					currTribute.push(randomTribute);
+					tributes.delete(randomTribute);
+				}
+
+				results.push(this.parseEvent(event.text, currTribute));
+				mentionResults.push(this.parseEvent(event.text, currTribute, true));
+				avatarsPerEvent.push(currTribute.map(trib => trib.avatar));
+			}
+		}
 	},
 
 	async generateStatusImage(ctx, tributeData, client) {
@@ -191,15 +364,15 @@ module.exports = {
 			const districtText = `District ${i + 1}`;
 
 			if (tributeArray.length === 2) {
-				textDestinationX = avatarPaddingX + halfAvatar + (avatarSpacingX * i) + (avatarSize * i);
+				textDestinationX = avatarPaddingX + halfAvatar + ((avatarSpacingX + avatarSize) * i);
 			} else if (i === districtCount - 1 && tributeArray.length % 2 === 1) {
-				textDestinationX = avatarPaddingX + (avatarSpacingX * xMultiplier) + (avatarSize * xMultiplier) + halfAvatar;
+				textDestinationX = avatarPaddingX + ((avatarSpacingX + avatarSize) * xMultiplier) + halfAvatar;
 			} else {
 				xMultiplier += 2;
 			}
 
 			ctx.fillText(districtText, textDestinationX, textDestinationY);
-			textDestinationX += (avatarSize * 2) + (avatarSpacingX * 2);
+			textDestinationX += (avatarSize + avatarSpacingX) * 2;
 
 			if ((i + 1) % 3 === 0) {
 				textDestinationX = initialInBetweenPosition;
@@ -209,7 +382,7 @@ module.exports = {
 		}
 	},
 
-	drawHeaderText(ctx, eventText, resultsText) {
+	drawHeaderText(ctx, resultsText, eventText) {
 		if (!eventText) eventText = 'The Reaping';
 
 		const text = ['The Hunger Games', eventText];
@@ -238,5 +411,33 @@ module.exports = {
 			ctx.fillText(text[i], ctx.canvas.width / 2, textPaddingY);
 			textPaddingY += 70;
 		}
+	},
+
+	async generateEventImage(client, eventText, resultsText, avatarArray) {
+		const canvasHeight = 500;
+		const canvas = createCanvas(1, canvasHeight);
+		const ctx = canvas.getContext('2d');
+
+		ctx.font = '35px arial';
+		ctx.fillStyle = '#FFFFFF';
+		const canvasWidth = ctx.measureText(resultsText).width + 100;
+		ctx.canvas.width = canvasWidth;
+
+		client.canvas.drawBackground(ctx, '#5d5050');
+		this.drawHeaderText(ctx, resultsText, eventText);
+
+		const avatarYPosition = (canvasHeight / 2) + 10;
+		let avatarXPosition = (canvasWidth / 2) - halfAvatar;
+		avatarXPosition -= ((avatarSpacingX / 2) + halfAvatar) * (avatarArray.length - 1);
+
+		for (let i = 0; i < avatarArray.length; i++) {
+			const tributeImage = await loadImage(avatarArray[i]);
+			ctx.drawImage(tributeImage, avatarXPosition, avatarYPosition, avatarSize, avatarSize);
+			ctx.strokeRect(avatarXPosition, avatarYPosition, avatarSize, avatarSize);
+
+			avatarXPosition += avatarSpacingX + avatarSize;
+		}
+
+		return canvas;
 	}
 };
