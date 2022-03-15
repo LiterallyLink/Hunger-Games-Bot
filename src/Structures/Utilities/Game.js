1e6b3e0e8ab233ff16c0e9ba5009a1163ffdd61e
LinkWasTaken <zechariahpeoples@gmail.com>
2022-03-15 13:00:02 -0700

    Minor changes

--- src/Structures/Utilities/Game.js
+++ src/Structures/Utilities/Game.js
@@ -5,10 +5,10 @@ module.exports = class Game {
 	}
 
 	gameOver(tributeData) {
-		const tributesLeftAlive = tributeData.filter(tribute => tribute.alive).length;
+		const tributesLeftAlive = tributeData.filter(tribute => tribute.alive);
 
-		if (tributesLeftAlive === 2) return tributesLeftAlive[0].district === tributesLeftAlive[1].district;
-		else if (tributesLeftAlive === 1) return true;
+		if (tributesLeftAlive.length === 2) return tributesLeftAlive[0].district === tributesLeftAlive[1].district;
+		else if (tributesLeftAlive.length === 1) return true;
 		else return false;
 	}
 
