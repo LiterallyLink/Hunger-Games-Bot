03b918ff3493271a522ffa1fcd30c1805d90d9ef
LinkWasTaken <zechariahpeoples@gmail.com>
2022-03-16 22:27:47 -0700

    Update Canvas.js
    Moved greyscale function to canvas for future uses.

--- src/Structures/Utilities/Canvas.js
+++ src/Structures/Utilities/Canvas.js
@@ -13,13 +13,28 @@ module.exports = class Canvas {
 
 	async massLoadImages(tributeData) {
 		const avatarPromises = [];
-
 		for (let i = 0; i < tributeData.length; i++) {
 			const avatar = loadImage(tributeData[i].avatar);
 			avatarPromises.push(avatar);
 		}
 
 		return await Promise.all(avatarPromises);
+	}
+
+	greyScale(ctx, destinationX, destinationY, avatarSize) {
+		const imageData = ctx.getImageData(destinationX, destinationY, avatarSize, avatarSize);
+
+		const pixels = imageData.data;
+
+		for (let j = 0; j < pixels.length; j += 4) {
+			const lightness = parseInt((pixels[j] + pixels[j + 1] + pixels[j + 2]) / 3);
+
+			pixels[j] = lightness;
+			pixels[j + 1] = lightness;
+			pixels[j + 2] = lightness;
+		}
+
+		ctx.putImageData(imageData, destinationX, destinationY);
 	}
 
 };
